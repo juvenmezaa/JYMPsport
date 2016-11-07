b481a08fd78b88579847bf7100520d2c45f96a78
Jorge <jlts.01.94@gmail.com>
2016-11-07 11:27:23 -0700

    Refinamiento del correo de confirmacion

--- resources/views/principalUser.blade.php
+++ resources/views/principalUser.blade.php
@@ -38,13 +38,17 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{Auth::User()->name}} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
+                            @if(Auth::User()->type==1)
+                            <li><a href="#">Administrador</a></li>
+                            @endif
+                            <li class="divider"></li>
                             <li>
                                 <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar Sesión</a>
                                 <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
                             </li>
-                            <li class="divider"></li>
+                            <!-- <li class="divider"></li> -->
                         </ul>
                     </li>
                     @else

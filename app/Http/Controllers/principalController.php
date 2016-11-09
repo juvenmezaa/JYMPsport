c66a8eb123a3a9c2fd8faf789989c67a9e38cd4e
juvenmezaa <juvenmezaa@hotmail.com>
2016-11-09 00:07:51 -0700

    Comenzando para visualizar un solo producto

--- app/Http/Controllers/principalController.php
+++ app/Http/Controllers/principalController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\productosModel;
 
 class principalController extends Controller
 {
     public function index(){
     	return view('principalUser');
+    }
+    public function producto($id){
+    	$producto=productosModel::find($id);
+    	return view('producto', compact('producto'));
     }
 }

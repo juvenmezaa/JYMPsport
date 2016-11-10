9d085a79d732bf4b973e6282479aa53c6c7b9dd5
Jorge <jlts.01.94@gmail.com>
2016-11-09 22:35:39 -0700

    Registrar productos desde archivo CSV

--- app/productosModel.php
+++ app/productosModel.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class productosModel extends Model
 {
     protected $table = "productos";
+
+    protected $fillable = ['id','descripcion','precio','costo','cantidad','talla','color'];
 }

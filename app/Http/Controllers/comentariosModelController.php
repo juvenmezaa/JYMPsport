c8ee541e261b42fb7539a730203cbedbc4ad518f
Jorge <jlts.01.94@gmail.com>
2016-11-14 15:17:39 -0700

    Mostrar todos los comentarios en dashboard admin

--- app/Http/Controllers/comentariosModelController.php
+++ app/Http/Controllers/comentariosModelController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use \Serverfireteam\Panel\CrudController;
-
+use DB;
 use Illuminate\Http\Request;
 
 class comentariosModelController extends CrudController{
@@ -13,42 +13,63 @@ class comentariosModelController extends CrudController{
     public function all($entity){
         parent::all($entity); 
 
+        $this->filter = \DataFilter::source(new \App\comentariosModel());
+        $this->filter->add('id', 'ID', 'text');
+        $this->filter->add('id_usuario', 'Usuario', 'text');
+        $this->filter->add('id_producto', 'Producto', 'text');
+        $this->filter->add('fecha', 'Fecha de Publicación', 'date');
+        $this->filter->add('comentario', 'Comentario', 'text');
+
+        $this->filter->submit('search');
+        $this->filter->reset('reset');
+        $this->filter->build();
+
+        $this->grid = \DataGrid::source($this->filter);
+        $this->grid->add('id','ID', true)->style("width:100px");
+        $this->grid->add('comentario', 'Comentario');
         
-        /** Simple code of  filter and grid part , List of all fields here : http://laravelpanel.com/docs/master/crud-fields
+        //$usuario = DB::table('users AS u')->join('comentarios AS c', 'c.id_usuario','=','u.id')->select('u.name')->get();
+        $this->grid->add('id_usuario','Usuario');
+        $usuario = $this->grid->getColumn('id_usuario');
 
+        $user = $usuario->name;
+        //dd($usuario);
+        $productos = array();
 
-			$this->filter = \DataFilter::source(new \App\Category);
-			$this->filter->add('name', 'Name', 'text');
-			$this->filter->submit('search');
-			$this->filter->reset('reset');
-			$this->filter->build();
+        //$productos = \App\comentariosModel::pluck("","id")->all();
+        //$this->grid->add('{{ implode(", ", $roles->pluck("name")->all()) }}', 'Role');
+        //$this->grid->add('{{ implode(" ", DB::table("users")->pluck("name","id")->all()) }}','Usuario');
+        //$this->grid->add('{{ DB::table("users AS U")->join("comentarios AS C","U.id","=","C.id_usuario")->pluck("U.name")->all() }}','UsuarioNombre');
+        $this->grid->add('id_producto','Producto');
 
-			$this->grid = \DataGrid::source($this->filter);
-			$this->grid->add('name', 'Name');
-			$this->grid->add('code', 'Code');
-			$this->addStylesToGrid();
+        $this->grid->paginate(10);
 
-        */
-                 
+        $this->addStylesToGrid();
+    
         return $this->returnView();
     }
     
     public function  edit($entity){
         
         parent::edit($entity);
-
-        /* Simple code of  edit part , List of all fields here : http://laravelpanel.com/docs/master/crud-fields
-	
-			$this->edit = \DataEdit::source(new \App\Category());
-
-			$this->edit->label('Edit Category');
-
-			$this->edit->add('name', 'Name', 'text');
 		
-			$this->edit->add('code', 'Code', 'text')->rule('required');
-
-
-        */
+		$this->edit = \DataEdit::source(new \App\comentariosModel());
+	
+        $this->edit->label('Editar Comentario');
+        $this->edit->add('descripcion','Descripción','text')->rule('required');
+        $this->edit->add('precio','Precio','text')->rule('required');
+        $this->edit->add('costo','Costo','text')->rule('required');
+        $this->edit->add('cantidad','Cantidad','text')->rule('required');
+        $this->edit->add('talla','Talla','select')->options(\App\Tallas::pluck("talla","id")->all())->rule('required');
+        //$tallas = DB::table('tallas')->select('id','talla')->get();
+        //$this->edit->add('talla','Talla','select')->options($tallas);
+        $this->edit->add('color','Color','text')->rule('required');
+        $this->edit->add('imagen','Imagen','image')->move(public_path().'/img/productos','')->preview(80,80);
+        $generos = array();
+        $generos["0"] = "Mujer";
+        $generos["1"] = "Hombre";
+        //dd($generos);
+        $this->edit->add('genero','Genero','select')->options($generos)->rule('required');
        
         return $this->returnEditView();
     }    
